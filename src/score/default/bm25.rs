e3aa9076ee6168e8f355631fb45e4f044f859d77
muji <muji@tmpfs.org>
2022-12-31 10:56:20 +0800

    Run fmt and fix clippy warnings.

--- src/score/default/bm25.rs
+++ src/score/default/bm25.rs
@@ -71,7 +71,7 @@ impl<T: Debug> ScoreCalculator<T, BM25TermCalculations> for BM25 {
         let pre_calculations = &before_output.unwrap(); // it will exist as we need BM25 parameters
         let mut score: f64 = 0_f64;
         for x in 0..document_details.field_length.len() {
-            let mut tf = (&document_pointer.term_frequency[x]).to_owned() as f64;
+            let mut tf = document_pointer.term_frequency[x] as f64;
             if tf > 0_f64 {
                 // calculating BM25 tf
                 let field_length = &document_details.field_length[x];
